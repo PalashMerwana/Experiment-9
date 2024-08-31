bc56f636ab9c14e374a0eb5c6507817d751b9578
PalashMerwana <palash.merwana.btech2023@sitpune.edu.in>
2024-08-31 12:18:13 +0530

    Add files via upload

---
+++ pointer_1.cpp
@@ -0,0 +1,27 @@
+
+//Palash Merwana 
+//Prn: 23070123095
+// experiment 9
+
+#include <bits/stdc++.h> 
+using namespace std;
+void geeks()
+{
+    int var = 5;
+
+    int* ptr; 
+
+    ptr = &var;
+
+    cout<<"Value at ptr = "<<ptr<<"\n";
+    cout<<"Value at var = " <<var<<"\n";
+    cout<<"Value at *ptr = "<<*ptr<<"\n";
+
+}
+
+
+int main()
+{
+    geeks();
+    return 0;
+} 
